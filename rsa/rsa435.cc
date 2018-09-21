2f2d485d03cdf32370495e17c773c21906d93687
kangere <kangere721@gmail.com>
2018-09-21 15:01:33 -0400

    Public, Private Key generation implemented

--- rsa/rsa435.cc
+++ rsa/rsa435.cc
@@ -5,16 +5,16 @@
 #include <iostream>
 #include <stdlib.h> 
 #include <time.h>
-#include <assert.h>
-#include <vector>
+#include <fstream>
 
 
 // 'BigIntegerLibrary.hh' includes all of the library headers.
 #include "BigIntegerLibrary.hh"
 
 BigInteger prime_gen();
-bool fermat_test(BigInteger&);
-
+bool fermat_test(const BigInteger&);
+void keygen();
+bool save_key(const std::string&, const BigInteger&, const BigInteger&);
 
 int main(){
 	/* The library throws 'const char *' error messages when things go
@@ -24,8 +24,9 @@ int main(){
 	
 	
 	try {
-		std::cout << prime_gen() << std::endl;
+		keygen();
 
+		
 		/*for(int i = 1; i < 23; i++)
 			std::cout << modexp(i,22,23) << std::endl;*/
       /*std::cout << "a couple of test cases for 3460:435/535 Algorithms!!!\n";
@@ -62,6 +63,8 @@ BigInteger prime_gen() {
 
 	BigInteger prime;
 
+	clock_t t = clock();
+
 	do{
 		prime  = BigInteger(1);
 
@@ -69,22 +72,77 @@ BigInteger prime_gen() {
         	prime = prime*10 +rand(); 	
 		}	
 
+		t = clock() - t;
+		std::cout << "Generating prime number, time elapsed " << ((float)t) / CLOCKS_PER_SEC << "s ..." << std::endl;
+	
 	}while(!(fermat_test(prime)));
 	
 	
 	return prime;
 }
 
-bool fermat_test(BigInteger &num) {
+bool fermat_test(const BigInteger &num) {
 
 	BigUnsigned exponent = BigUnsigned(num.getMagnitude()) - BigUnsigned(1);
 
 	for(int i = 2; i < 5; i++){
 		if(modexp(BigInteger(i), exponent, BigUnsigned(num.getMagnitude())) != 1){
-			std::cout <<"Is Not Prime " << std::endl;
 			return false;
-			
 		}
+	}
+
+	return true;
+}
+
+void keygen() {
+
+	BigInteger p = prime_gen();
+
+	BigInteger q = prime_gen();
+
+	std::cout << "P = " << p << std::endl;
+	std::cout << "Q = " << q << std::endl;
+
+	if(save_key("p_q.txt", p,q)){
+		std::cout << "file p_q.txt saved" <<std::endl;
+	}
+
+	BigInteger n = p * q;
+
+	BigInteger e = 65537;
+
+	BigUnsigned totient_n = BigUnsigned(p.getMagnitude() - BigUnsigned(1)) * 
+					BigUnsigned(q.getMagnitude() - BigUnsigned(1));
+
+	BigUnsigned d = modinv(e,totient_n);
+
+	if(save_key("e_n.txt",e,n)){
+		std::cout << "file e_n.txt saved" << std::endl;
+	}
+
+	if(save_key("d_n.txt",d,n)){
+		std::cout << "file d_n.txt saved" << std::endl;
+	}
+
+}
+
+
+bool save_key(const std::string& filename, const BigInteger& first, const BigInteger& second) {
+
+	if(filename.empty()){
+		std::cout << "filename is empty " << std::endl;
+		return false;
+	}
+
+	std::fstream out (filename.c_str(), std::fstream::out | std::fstream::trunc);
+
+	if(out.is_open()){
+		out << first << std::endl;
+		out << second ;
+
+		out.close();
+	} else {
+		return false;
 	}
 
 	return true;
